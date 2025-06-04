976d5998cf4bcf6a31a944a934df3a0de8b6a3d7
andrewwkimm <andrewkimka@gmail.com>
2025-06-04 19:32:38 +0000

    Set root route

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Add default URL to environment
+  config.action_mailer.default_url_options = { host: 'suminote.onrender.com' }
 end

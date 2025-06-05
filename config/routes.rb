d87b8c1aa4cc46cd350cc1a5f9d55194eec45482
andrewwkimm <andrewkimka@gmail.com>
2025-06-05 04:32:27 +0000

    Route the landing page to the home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'signin',
+    sign_out: 'signout',
+    sign_up: 'signup'
+  }
   resources :notes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "notes#index"
+  root "home#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
